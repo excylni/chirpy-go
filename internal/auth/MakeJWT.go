846dd3ae6b974453412887e1b646940a66aaa0ec
excylni <191880022+excylni@users.noreply.github.com>
2026-08-29 17:29:36 +0200

    added JWT authentification

---
+++ internal/auth/MakeJWT.go
@@ -0,0 +1,59 @@
+package auth
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	method := jwt.SigningMethodHS256
+	claims := jwt.RegisteredClaims{
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn)),
+		IssuedAt: jwt.NewNumericDate(time.Now()),
+		Issuer: "chirpy-access",
+		Subject: userID.String(),
+	}
+
+	token := jwt.NewWithClaims(method, claims)
+	ss, err := token.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", err
+	}
+	return ss, nil 	
+}
+
+func validateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	// parsing the token
+	claims := &jwt.RegisteredClaims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims, func (token *jwt.Token) (any, error){
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(tokenSecret), nil
+	})
+
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	if !token.Valid {
+		return uuid.Nil, fmt.Errorf("invalid token")
+	}
+
+	userIDString, err := token.Claims.GetSubject()
+	if err != nil || userIDString == "" {
+		return uuid.Nil, fmt.Errorf("subject claim missing")
+	}
+
+	userID, err := uuid.Parse(userIDString)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	return userID, nil
+}
